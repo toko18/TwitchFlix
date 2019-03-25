63382bc8ae5059853b1668c4d592514a8dced3dd
toko18 <35262948+toko18@users.noreply.github.com>
2019-03-25 17:38:26 +0000

    Toolbar added

---
+++ app/src/main/java/com/toko/twitchflix/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.toko.twitchflix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
